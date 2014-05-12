18bfbcade780ea80be26892a12538264e05938da
Michael Bauer <michael@m-bauer.org>
2014-05-12 19:59:23 +0200

    Add Many2Many relation with intermediary model.

--- pizza/admin.py
+++ pizza/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from pizza.models import Pizza, Topping
+from pizza.models import Pizza, Topping, ToppingUsage
 
-admin.site.register(Pizza)
+class ToppingUsageInline(admin.StackedInline):
+    model = ToppingUsage
+
+class PizzaAdmin(admin.ModelAdmin):
+    inlines = [ToppingUsageInline,]
+
+admin.site.register(Pizza, PizzaAdmin)
 admin.site.register(Topping)
+admin.site.register(ToppingUsage)
--- pizza/models.py
+++ pizza/models.py
@@ -13,8 +13,17 @@ class Pizza(models.Model):
         return self.name
 
 class Topping(models.Model):
-    pizza = models.ForeignKey(Pizza)
+    pizza = models.ManyToManyField(Pizza, through='ToppingUsage')
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
+class ToppingUsage(models.Model):
+    pizza = models.ForeignKey(Pizza)
+    topping = models.ForeignKey(Topping)
+    amount = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return "%d units of %s" % (self.amount, self.topping)
+
