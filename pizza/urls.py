fbf958c985452c921f058659a27930a894742cb0
Michael Bauer <michael@m-bauer.org>
2014-05-14 11:00:54 +0200

    Use same view for creating and updating pizzas.

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, url
 
 from pizza import views
-from pizza.views import PizzaCreate, PizzaUpdate, PizzaDelete, PizzaDetail, PizzaList
+from pizza.views import PizzaEdit, PizzaUpdate, PizzaDelete, PizzaDetail, PizzaList
 
 urlpatterns = patterns('',
 
     url(r'^list/$', PizzaList.as_view(), name='pizza_list'),
-    url(r'^add/$', PizzaCreate.as_view(), name='pizza_add'),
-    url(r'^(?P<pk>\d+)/update/$', PizzaUpdate.as_view(), name='pizza_update'),
+    url(r'^add/$', PizzaEdit.as_view(), name='pizza_add'),
+    url(r'^(?P<pk>\d+)/update/$', PizzaEdit.as_view(), name='pizza_update'),
     url(r'^(?P<pk>\d+)/delete/$', PizzaDelete.as_view(), name='pizza_delete'),
     url(r'^(?P<pk>\d+)/$', PizzaDetail.as_view(), name='pizza_detail'),
 )
--- pizza/views.py
+++ pizza/views.py
@@ -17,7 +17,7 @@ class PizzaDetail(DetailView):
     context_object_name = 'pizza'
     model = Pizza
 
-class PizzaCreate(TemplateView):
+class PizzaEdit(TemplateView):
     template_name = 'pizza/pizza_form.html'
 
     def get(self, request, *args, **kwargs):
@@ -28,60 +28,15 @@ class PizzaCreate(TemplateView):
          - [Toppingform to create a new Topping]
          - Set of Toppingusages to specify the usage parameters
         """
-        pizza_form = PizzaForm(instance = None)
-        topping_usage_formset = ToppingUsageFormSet(instance=None)
-        return self.render_to_response(self.get_context_data(
-            pizza_form=pizza_form,
-            topping_usage_formset=topping_usage_formset
-        ))
-
-    def post(self, request, *args, **kwargs):
-        """
-        Handles POST requests, instantiating a form instance with the passed
-        POST variables and then checked for validity.
-
-        Things to do:
-        - Check if all forms are valid
-        - Save the pizza
-        [- Save the topping]
-        - Set topping and pizza PK to ToppingUsage
-        - Save toppingUsage
-        """
-        pizza_form = PizzaForm(data=self.request.POST)
-        topping_usage_formset = ToppingUsageFormSet(data=self.request.POST)
-        if pizza_form.is_valid() and topping_usage_formset.is_valid():
-            # save pizza
-            pizza = pizza_form.save()
-            # Set pizza instance for toppingusageset before saving it
-            topping_usage_formset.instance = pizza
-            # Save toppingusageset
-            topping_usage_formset.save()
-            # return pizza url
-            return HttpResponseRedirect(pizza.get_absolute_url())
-        else:
-            return self.render_to_response(self.get_context_data(
-                pizza_form=pizza_form,
-                topping_usage_formset=topping_usage_formset
-            ))
-
-class PizzaUpdate(TemplateView):
-    template_name = 'pizza/pizza_form.html'
-
-    def get(self, request, *args, **kwargs):
-        """
-        Handles GET requests and instantiates a blank version of the forms.
-        Should display 3 parts:
-         - Pizzaform to create a new pizza
-         - [Toppingform to create a new Topping]
-         - Set of Toppingusages to specify the usage parameters
-        """
+        pizza = None
         pk = self.kwargs.get('pk', None)
-        try:
-            # Get the single item from the filtered queryset
-            pizza = Pizza.objects.get(pk=pk)
-        except ObjectDoesNotExist:
-            raise Http404(_("No %(verbose_name)s found matching the query") %
-                          {'verbose_name': Pizza._meta.verbose_name})
+        if pk:
+            try:
+                # Get the single item from the filtered queryset
+                pizza = Pizza.objects.get(pk=pk)
+            except ObjectDoesNotExist:
+                raise Http404("No %(verbose_name)s found matching the query" %
+                              {'verbose_name': Pizza._meta.verbose_name})
 
         pizza_form = PizzaForm(instance = pizza)
         topping_usage_formset = ToppingUsageFormSet(instance=pizza)
@@ -103,19 +58,23 @@ class PizzaUpdate(TemplateView):
         - Set topping and pizza PK to ToppingUsage
         - Save toppingUsage
         """
+        pizza = None
         pk = self.kwargs.get('pk', None)
-        try:
-            # Get the single item from the filtered queryset
-            pizza = Pizza.objects.get(pk=pk)
-        except ObjectDoesNotExist:
-            raise Http404(_("No %(verbose_name)s found matching the query") %
-                          {'verbose_name': Pizza._meta.verbose_name})
-
+        if pk:
+            try:
+                # Get the single item from the filtered queryset
+                pizza = Pizza.objects.get(pk=pk)
+            except ObjectDoesNotExist:
+                raise Http404("No %(verbose_name)s found matching the query" %
+                              {'verbose_name': Pizza._meta.verbose_name})
         pizza_form = PizzaForm(data=self.request.POST, instance=pizza)
         topping_usage_formset = ToppingUsageFormSet(data=self.request.POST, instance=pizza)
         if pizza_form.is_valid() and topping_usage_formset.is_valid():
             # save pizza
             pizza = pizza_form.save()
+            # Set pizza instance for toppingusageset before saving it
+            # This is only necessary for creating new pizza, but should not harm anyway...
+            topping_usage_formset.instance = pizza
             # Save toppingusageset
             topping_usage_formset.save()
             # return pizza url
